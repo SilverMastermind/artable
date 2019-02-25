b4657349b515c5d04b52ada1f3bc4e6041708b74
Marcus Van Den Berg <marcusvandenberg@silverninja-mac.local>
2019-02-25 22:58:57 +0200

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Marcus Van Den Berg on 2019/02/25.
+//  Copyright © 2019 SilverNinja™. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
