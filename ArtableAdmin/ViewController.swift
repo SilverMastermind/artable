b8d93c3be3c37986fab897c285bccee99c44a05b
Marcus Van Den Berg <marcusvandenberg@SilverNinja-Mac.local>
2019-02-25 23:23:00 +0200

    Initial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Marcus Van Den Berg on 2019/02/25.
+//  Copyright © 2019 SilverNinja™. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
